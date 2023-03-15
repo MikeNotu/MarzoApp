74cadb2ad598c65949fa93bff9cecc92aa1825dc
MikeNotu <mikebateman98@gmail.com>
2023-03-15 06:27:00 -0600

    Finished Bienvenido Component

--- Utils/constants.ts
+++ Utils/constants.ts
@@ -1,1 +1,5 @@
 export const BASE_URL = 'https://6222994f666291106a29f999.mockapi.io/api/v1';
+
+export const COLORS = {
+  BLACK: '#020202',
+};
--- components/Bienvenido.tsx
+++ components/Bienvenido.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import {COLORS} from '../utils/constants';
+import {CustomText} from './CustomText';
 interface PersonData {
   firstName: String;
   lastName: String;
@@ -8,17 +10,31 @@ interface PersonData {
 export const Bienvenido = ({firstName, lastName}: PersonData): JSX.Element => {
   return (
     <View>
-      <View
-        style={{
-          marginLeft: 20,
-          marginTop: 24,
-          backgroundColor: '#cc22ff',
-          width: 209,
-          height: 48,
-        }}>
-        <Text style={{fontFamily: 'Avenir'}}>name: {firstName}</Text>
-        <Text>Herreee: {lastName}</Text>
+      <View style={styles.container}>
+        <CustomText
+          content={'Bienvenido de vuelta!'}
+          size={20}
+          weight={'800'}
+          color={COLORS.BLACK}
+        />
+        <CustomText
+          content={`${firstName} ${lastName}`}
+          size={16}
+          weight={'400'}
+          color={COLORS.BLACK}
+          marginTop={-5}
+        />
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    paddingLeft: 1,
+    marginLeft: 20,
+    marginTop: 24,
+    width: 209,
+    height: 48,
+  },
+});
